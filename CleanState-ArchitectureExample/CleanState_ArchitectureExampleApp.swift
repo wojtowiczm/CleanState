3cb9bf1d92efb332d3b3ca0eb8101ff37498a89f
Michał Wójtowicz <wojtowiczmichal97@gmail.com>
2020-12-28 17:10:15 +0100

    Initial Commit

---
+++ CleanState-ArchitectureExample/CleanState_ArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanState_ArchitectureExampleApp.swift
+//  CleanState-ArchitectureExample
+//
+//  Created by private on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CleanState_ArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanState-ArchitectureExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanState-ArchitectureExample
+//
+//  Created by private on 28/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
